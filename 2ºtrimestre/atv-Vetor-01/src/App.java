c9e87a7e66cf20ba3ab788423b1a02a28c8f7929
Vitor Klein <vitorhklein2@gmail.com>
2021-06-14 10:56:29 -0300

    adjust sintaxe

--- 2ºtrimestre/atv-Vetor-01/src/App.java
+++ 2ºtrimestre/atv-Vetor-01/src/App.java
@@ -5,6 +5,9 @@ public class App {
 		int vet[]; 
 		vet = new int[12];
 		int soma=0;
+		int multi = 0;
+		int med = 0;
+
 		for(int i=0; i<12 ;i++) {
 			vet[i]=Integer.parseInt(JOptionPane.showInputDialog("Informar um Numero"));
 		}
@@ -17,12 +20,10 @@ public class App {
 			soma =  soma + vet[c];
 		}
 
-		int multi = 0;
 		for(int m=0; m<vet.length; m++) {
 			multi = multi * vet[m];
 		}
 
-		int med = 0;
 		for(int i = 0; i < vet.length; i++){
             med = (med + vet[i])/12;
             
