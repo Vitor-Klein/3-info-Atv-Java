4f83d3382efb3553e06ebf0fbd40ee958e26f501
Vitor Klein <vitorhklein2@gmail.com>
2021-09-28 19:12:14 -0300

    finish the livro props

--- 2ºtrimestre/atv-interface02/src/base/TesteLivro.java
+++ 2ºtrimestre/atv-interface02/src/base/TesteLivro.java
@@ -4,5 +4,12 @@ public class TesteLivro {
   private static void main(String[] args){
     Livro[] livros = new Livro[5];
     livros[0] = new Livro("Willyan", "Romeu e Julieta");
+    livros[1] = new Livro("Vitor", "Brilho Eterno");
+    livros[2] = new Livro("J.K.Rolling", "Harry Potter");
+
+    for(int i = 0; i < livros.length; i++){
+      livros[i].ordenar(livros);
+      System.out.println("");
+    }
   }
 }
