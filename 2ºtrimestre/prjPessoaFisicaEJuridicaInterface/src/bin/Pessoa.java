87398c40693f1b8d0821a8a352b7651f7940f816
Vitor Klein <vitorhklein2@gmail.com>
2021-09-28 19:17:09 -0300

    create pessoa fisica e juridica package

---
+++ 2ºtrimestre/prjPessoaFisicaEJuridicaInterface/src/bin/Pessoa.java
@@ -0,0 +1,51 @@
+package bin;
+
+public class Pessoa {
+
+
+  private String nome ;
+  private double altura;
+  private int idade;
+  private String endereco, telefone;
+  
+  
+  public Pessoa(String nome, double altura, int idade, String endereco, String telefone) {
+  this.nome = nome;
+  this.altura = altura;
+  this.idade = idade;
+  this.endereco = endereco;
+  this.telefone = telefone;
+  }
+  public String getNome() {
+  return nome;
+  }
+  public void setNome(String nome) {
+  this.nome = nome;
+  }
+  public double getAltura() {
+  return altura;
+  }
+  public void setAltura(double altura) {
+  this.altura = altura;
+  }
+  public int getIdade() {
+  return idade;
+  }
+  public void setIdade(int idade) {
+  this.idade = idade;
+  }
+  public String getEndereco() {
+  return endereco;
+  }
+  public void setEndereco(String endereco) {
+  this.endereco = endereco;
+  }
+  public String getTelefone() {
+  return telefone;
+  }
+  public void setTelefone(String telefone) {
+  this.telefone = telefone;
+  }
+  
+  }
+  
---
+++ 2ºtrimestre/prjPessoaFisicaEJuridicaInterface/src/bin/PessoaFisica.java
@@ -0,0 +1,26 @@
+package bin;
+
+public class PessoaFisica extends Pessoa {
+
+  private String cpf;
+  
+  public PessoaFisica(String nome, double altura, int idade, 
+  String endereco, String telefone, String CPF) {
+  super(nome, altura, idade, endereco, telefone);
+  this.cpf = CPF; 
+  }
+  
+  
+  public String getCpf() {
+  return cpf;
+  }
+  
+  
+  public void setCpf(String cpf) {
+  this.cpf = cpf;
+  }
+  
+  
+  
+  
+  }
