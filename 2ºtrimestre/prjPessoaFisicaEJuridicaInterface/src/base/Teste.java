70ea7cf93f4d4a415bbc4c4fbe08dfeb00df0ef6
Vitor Klein <vitorhklein2@gmail.com>
2021-09-28 19:49:51 -0300

    start crud

--- 2ºtrimestre/prjPessoaFisicaEJuridicaInterface/src/base/Teste.java
+++ 2ºtrimestre/prjPessoaFisicaEJuridicaInterface/src/base/Teste.java
@@ -3,5 +3,11 @@ package base;
 public class Teste {
   public static void main(String[] args){
     PessoaJuridica pj = new PessoaJuridica();
+    pj.setNome("Vitor");
+    pj.setAltura(1.75);
+    pj.setEndereco("Rua Santa Cruz");
+    pj.setIdade(17);
+    pj.setCnpj("1222834834");
+    pj.imprime();
   }
 }
