fad8ab41d65048910cabd431306aba1e85e98602
Vitor Klein <vitorhklein2@gmail.com>
2021-09-28 19:22:04 -0300

    create pessoa juridica class

--- 2ºtrimestre/prjPessoaFisicaEJuridicaInterface/src/base/PessoaJuridica.java
+++ 2ºtrimestre/prjPessoaFisicaEJuridicaInterface/src/base/PessoaJuridica.java
@@ -1,5 +1,31 @@
 package base;
 
-public class PessoaJuridica {
+public class PessoaJuridica extends Pessoa {
+  private String IE;
+  private String cnpj;
+
+  public PessoaJuridica(String nome, double altura, int idade, 
+  String endereco, String telefone, String CPF){
+    super(nome, altura, idade, endereco, telefone);
+    this.IE = IE;     
+    this.cnpj = cnpj;   
+  }
+
+  public String getIE() {
+    return IE;
+  }
+
+  public void setIE(String iE) {
+    IE = iE;
+  }
+
+  public String getCnpj() {
+    return cnpj;
+  }
+
+  public void setCnpj(String cnpj) {
+    this.cnpj = cnpj;
+  }
+
   
 }
